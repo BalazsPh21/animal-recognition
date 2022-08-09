4299f179fe10db2fab62ea179ff86e6e36fe8dd5
Balázs <balazsph@outlook.hu>
2022-08-09 16:50:59 +0200

    Update animal.py

--- animal.py
+++ animal.py
@@ -8,8 +8,8 @@ import cv2
 from sklearn.model_selection import train_test_split
 
 EPOCHS = 10
-IMG_WIDTH = 30
-IMG_HEIGHT = 30
+IMG_WIDTH = 1000
+IMG_HEIGHT = 1000
 NUM_CATEGORIES = 10
 TEST_SIZE = 0.4
 
