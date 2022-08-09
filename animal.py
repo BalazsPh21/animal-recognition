17d150edcdb6611c84fd0aed7ec766ad10aa5cf0
Balázs <balazsph@outlook.hu>
2022-08-09 16:22:27 +0200

    Update animal.py

--- animal.py
+++ animal.py
@@ -1,7 +1,16 @@
 import sys
 import tensorflow as tf
 import numpy as np
+import os
 import cv2
+
+from sklearn.model_selection import train_test_split
+
+EPOCHS = 10
+IMG_WIDTH = 30
+IMG_HEIGHT = 30
+NUM_CATEGORIES = 10
+TEST_SIZE = 0.4
 
 def main():
 
@@ -13,8 +22,53 @@ def main():
     images, labels = load_data(sys.argv[1])
 
     # Split data into training and testing sets
+    labels = tf.keras.utils.to_categorical(labels)
+    x_train, x_test, y_train, y_test = train_test_split(
+        np.array(images), np.array(labels), test_size=TEST_SIZE
+    )
 
+    # Get a compiled neural network
+    model = get_model()
+
+    # Fit model on training set
+    model.fit(x_train, y_train, epochs=EPOCHS)
+
+    # Evaluate NN performance
+    model.evaluate(x_test, y_test, verbose=2)
 
 
 def load_data(data_dir):
-    raise NotImplementedError
+    images = list()
+    labels = list()
+
+
+    return images, labels
+
+
+def get_model():
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Conv2D(
+            32, (3, 3), activation="sigmoid", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
+        ),
+
+        tf.keras.layers.MaxPooling2D(pool_size=(3, 3)),
+        
+        tf.keras.layers.Flatten(),
+
+        tf.keras.layers.Dense(256, activation="sigmoid"),
+        tf.keras.layers.Dropout(0.5),
+
+        tf.keras.layers.Dense(NUM_CATEGORIES, activation="softmax")
+    ])
+
+    model.compile(
+        optimizer="adam",
+        loss="categorical_crossentropy",
+        metrics=["accuracy"]
+    )
+
+    return model
+
+
+if __name__ == "__main__":
+    main()
